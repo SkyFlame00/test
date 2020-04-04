cb35fdbcd2c4efbb90237930673c4ec909d5ea60
skyflame <skyflame@yandex-team.ru>
2020-04-04 15:04:55 +0300

    fix

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const page = document.querySelector('.page');
-    const sticky = document.querySelector('.sticky');
-    const placeholder = document.querySelector('.placeholder');
     const action = document.querySelector('.action');
 
-    document.addEventListener('scroll', () => {
-        console.log(window.pageYOffset)
-        page.classList.add('page_pinned');
-        // if (window.pageYOffset > 0) {
-        //     page.classList.add('page_pinned');
-        // } else {
-            // page.classList.remove('page_pinned');
-        // }
-    });
-
     action.onclick = () => {
-        page.classList.remove('page_pinned');
+        page.classList.toggle('page_pinned');
     };
-
-    window.addEventListener('orientationchange', () => {
-        // page.classList.remove('page_pinned');
-    });
 });
